a3e516107b3293b6c4ffbb4f6416e76bd022e79a
unique-kadyan <rajeshsinghkadyan@gmail.com>
2021-04-15 23:37:24 +0530

    Mern_Stack

--- server/index.js
+++ server/index.js
@@ -1,4 +1,10 @@
 import express from 'express';
-import bodyparser from 'body-parser';
+import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
+constant app = express();
+app.use(bodyParser.jason({ limit: "20mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "20mb", extended: true }));
+
+app.use(cors());
